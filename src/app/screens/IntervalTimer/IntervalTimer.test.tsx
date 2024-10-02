39fc8e238953ca20a9065ca6aa70cb1dfa6e2615
Irfan Undevli <i.undevli@gmail.com>
2024-10-02 12:48:21 +0100

    Add linter & prettier

--- src/app/screens/IntervalTimer/IntervalTimer.test.tsx
+++ src/app/screens/IntervalTimer/IntervalTimer.test.tsx
@@ -1,70 +1,62 @@
-import {
-  act,
-  fireEvent,
-  render,
-  screen,
-  within,
-} from "@testing-library/react-native";
-import IntervalTimer from "./IntervalTimer";
-import { getIntervals } from "@/store";
+import { act, fireEvent, render, screen, within } from '@testing-library/react-native';
+import IntervalTimer from './IntervalTimer';
+import { getIntervals } from '@/store';
 
-jest.mock("@/store");
+jest.mock('@/store');
 jest.useFakeTimers();
 
 const SECOND = 1000;
 
-describe("Interval Timer Component", () => {
-  afterEach(() => {
-    jest.clearAllTimers();
-  });
-
-  const advanceTimersByTime = (timeInMillis: number) => {
-    act(() => {
-      jest.advanceTimersByTime(timeInMillis);
-    });
-  };
-
-  it("displays current interval countdown", () => {
-    (getIntervals as jest.Mock).mockReturnValue([
-      { type: "exercise", name: "Exercise", duration: 60 * SECOND },
-    ]);
-    render(<IntervalTimer />);
-
-    fireEvent.press(screen.getByTestId("play"));
-    advanceTimersByTime(2 * SECOND);
-
-    const currInterval = screen.getByTestId("current-interval");
-    expect(within(currInterval).getByText("Exercise")).toBeOnTheScreen();
-    expect(within(currInterval).getByText("00:58")).toBeOnTheScreen();
-  });
-
-  it("displays next interval", () => {
-    (getIntervals as jest.Mock).mockReturnValue([
-      { type: "exercise", name: "Exercise", duration: 60 * SECOND },
-      { type: "rest", name: "Rest", duration: 10 * SECOND },
-    ]);
-
-    render(<IntervalTimer />);
-
-    const nextInterval = screen.getByTestId("next-interval");
-    expect(within(nextInterval).getByText("Rest")).toBeOnTheScreen();
-    expect(within(nextInterval).getByText("00:10")).toBeOnTheScreen();
-  });
-
-  it("displays next interval within current interval section when the current interval is finished", () => {
-    (getIntervals as jest.Mock).mockReturnValue([
-      { type: "exercise", name: "Exercise", duration: 5 * SECOND },
-      { type: "rest", name: "Rest", duration: 2 * SECOND },
-    ]);
-    render(<IntervalTimer />);
-    act(() => {
-      fireEvent.press(screen.getByTestId("play"));
+describe('Interval Timer Component', () => {
+    afterEach(() => {
+        jest.clearAllTimers();
     });
 
-    advanceTimersByTime(5 * SECOND);
+    const advanceTimersByTime = (timeInMillis: number) => {
+        act(() => {
+            jest.advanceTimersByTime(timeInMillis);
+        });
+    };
 
-    const currInterval = screen.getByTestId("current-interval");
-    expect(within(currInterval).getByText("Rest")).toBeOnTheScreen();
-    expect(within(currInterval).getByText("00:02")).toBeOnTheScreen();
-  });
+    it('displays current interval countdown', () => {
+        (getIntervals as jest.Mock).mockReturnValue([{ type: 'exercise', name: 'Exercise', duration: 60 * SECOND }]);
+        render(<IntervalTimer />);
+
+        fireEvent.press(screen.getByTestId('play'));
+        advanceTimersByTime(2 * SECOND);
+
+        const currInterval = screen.getByTestId('current-interval');
+        expect(within(currInterval).getByText('Exercise')).toBeOnTheScreen();
+        expect(within(currInterval).getByText('00:58')).toBeOnTheScreen();
+    });
+
+    it('displays next interval', () => {
+        (getIntervals as jest.Mock).mockReturnValue([
+            { type: 'exercise', name: 'Exercise', duration: 60 * SECOND },
+            { type: 'rest', name: 'Rest', duration: 10 * SECOND },
+        ]);
+
+        render(<IntervalTimer />);
+
+        const nextInterval = screen.getByTestId('next-interval');
+        expect(within(nextInterval).getByText('Rest')).toBeOnTheScreen();
+        expect(within(nextInterval).getByText('00:10')).toBeOnTheScreen();
+    });
+
+    it('displays next interval within current interval section when the current interval is finished', () => {
+        (getIntervals as jest.Mock).mockReturnValue([
+            { type: 'exercise', name: 'Exercise', duration: 5 * SECOND },
+            { type: 'rest', name: 'Rest', duration: 2 * SECOND },
+        ]);
+        render(<IntervalTimer />);
+        act(() => {
+            fireEvent.press(screen.getByTestId('play'));
+        });
+
+        advanceTimersByTime(5 * SECOND);
+
+        const currInterval = screen.getByTestId('current-interval');
+        expect(within(currInterval).getByText('Rest')).toBeOnTheScreen();
+        expect(within(currInterval).getByText('00:02')).toBeOnTheScreen();
+    });
 });
--- src/app/screens/IntervalTimer/IntervalTimer.tsx
+++ src/app/screens/IntervalTimer/IntervalTimer.tsx
@@ -1,64 +1,59 @@
-import React, { useState } from "react";
-import { View, StyleSheet } from "react-native";
-import Header from "./header";
-import Footer from "./footer";
-import IntervalCard from "./card";
-import { useCountdown } from "@/hooks/countdown";
-import { getIntervals } from "@/store";
-import { millisecondsToTime } from "@/utils/time";
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import Header from './header';
+import Footer from './footer';
+import IntervalCard from './card';
+import { useCountdown } from '@/hooks/countdown';
+import { getIntervals } from '@/store';
+import { millisecondsToTime } from '@/utils/time';
 
 export default function IntervalTimer() {
-  const [currIntervalIdx, setCurrIntervalIdx] = useState(0);
-  const intervals = getIntervals();
-  const currInterval = intervals[currIntervalIdx];
-  const nextInterval = intervals[currIntervalIdx + 1];
+    const [currIntervalIdx, setCurrIntervalIdx] = useState(0);
+    const intervals = getIntervals();
+    const currInterval = intervals[currIntervalIdx];
+    const nextInterval = intervals[currIntervalIdx + 1];
 
-  const {
-    start: startCountdown,
-    time: currIntervalRemaining,
-    restart: restartCountdown,
-    isFinished: isCountdownFinished,
-  } = useCountdown(currInterval.duration);
+    const {
+        start: startCountdown,
+        time: currIntervalRemaining,
+        restart: restartCountdown,
+        isFinished: isCountdownFinished,
+    } = useCountdown(currInterval.duration);
 
-  if (isCountdownFinished) {
-    const nIdx = currIntervalIdx + 1;
-    const nInterval = intervals[nIdx];
-    if (nInterval) {
-      restartCountdown(nInterval.duration);
-      setCurrIntervalIdx(nIdx);
+    if (isCountdownFinished) {
+        const nIdx = currIntervalIdx + 1;
+        const nInterval = intervals[nIdx];
+        if (nInterval) {
+            restartCountdown(nInterval.duration);
+            setCurrIntervalIdx(nIdx);
+        }
     }
-  }
 
-  return (
-    <View style={styles.container}>
-      <Header />
+    return (
+        <View style={styles.container}>
+            <Header />
 
-      <IntervalCard
-        name={currInterval.name}
-        testID="current-interval"
-        time={currIntervalRemaining}
-        type={currInterval.type}
-      />
+            <IntervalCard name={currInterval.name} testID="current-interval" time={currIntervalRemaining} type={currInterval.type} />
 
-      {nextInterval && (
-        <IntervalCard
-          name={nextInterval.name}
-          testID="next-interval"
-          time={millisecondsToTime(nextInterval.duration)}
-          type={nextInterval.type}
-        />
-      )}
+            {nextInterval && (
+                <IntervalCard
+                    name={nextInterval.name}
+                    testID="next-interval"
+                    time={millisecondsToTime(nextInterval.duration)}
+                    type={nextInterval.type}
+                />
+            )}
 
-      <Footer onStart={startCountdown} />
-    </View>
-  );
+            <Footer onStart={startCountdown} />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "black",
-    flex: 1,
-    justifyContent: "space-between",
-    padding: 40,
-  },
+    container: {
+        backgroundColor: 'black',
+        flex: 1,
+        justifyContent: 'space-between',
+        padding: 40,
+    },
 });
