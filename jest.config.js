f082db5ba953136fd32e370288c117edf160a86a
Irfan Undevli <i.undevli@gmail.com>
2024-10-05 14:20:05 +0100

    Add stop button

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-  collectCoverage: true,
-  collectCoverageFrom: [
-    "**/*.{ts,tsx,js,jsx}",
-    "!**/coverage/**",
-    "!**/node_modules/**",
-    "!**/babel.config.js",
-    "!**/expo-env.d.ts",
-    "!**/.expo/**",
-  ],
-  moduleNameMapper: {
-    "@/hooks/(.*)": "<rootDir>/src/app/hooks/$1",
-    "@/store": "<rootDir>/src/app/store/",
-    "@/utils/(.*)": "<rootDir>/src/app/utils/$1",
-  },
-  preset: "jest-expo",
-  setupFilesAfterEnv: ["<rootDir>/setup-jest.js"],
-  transformIgnorePatterns: [
-    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg)",
-  ],
+    collectCoverage: true,
+    collectCoverageFrom: [
+        '**/*.{ts,tsx,js,jsx}',
+        '!**/coverage/**',
+        '!**/node_modules/**',
+        '!**/babel.config.js',
+        '!**/expo-env.d.ts',
+        '!**/.expo/**',
+    ],
+    moduleNameMapper: {
+        '@/hooks/(.*)': '<rootDir>/src/app/hooks/$1',
+        '@/store': '<rootDir>/src/app/store/',
+        '@/utils/(.*)': '<rootDir>/src/app/utils/$1',
+    },
+    preset: 'jest-expo',
+    setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+    transformIgnorePatterns: [
+        'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg)',
+    ],
 };
