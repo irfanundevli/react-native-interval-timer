f316f657684beb987938239217325dd3107edf34
Irfan Undevli <i.undevli@gmail.com>
2024-10-21 19:03:47 +0100

    Add design for current interval

--- src/app/screens/IntervalTimer/card/index.tsx
+++ src/app/screens/IntervalTimer/card/index.tsx
@@ -4,39 +4,54 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 interface Props {
-    name: string;
-    testID?: string;
-    time: string;
-    type: IntervalType;
+  name: string;
+  testID?: string;
+  time: string;
+  type: IntervalType;
 }
 
 export default function IntervalCard({ name, testID, time, type }: Props) {
-    const textColor = getTextColor(type);
+  const textColor = getTextColor(type);
 
-    return (
-        <View style={styles.container} testID={testID}>
-            <Text style={{ color: textColor }}>{name}</Text>
-            <Text style={{ color: textColor }}>{time}</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container} testID={testID}>
+      <View style={styles.content}>
+        <Text style={[{ color: textColor }, styles.name]}>{name}</Text>
+        <Text style={[{ color: textColor }, styles.time]}>{time}</Text>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        alignItems: 'center',
-    },
-    text: {
-        color: 'white',
-    },
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    padding: 8,
+  },
+  content: {
+    flex: 1,
+    alignItems: 'flex-end',
+  },
+  name: {
+    fontWeight: '200',
+    fontSize: 48,
+  },
+  time: {
+    textAlign: 'center',
+    fontWeight: '700',
+    fontSize: 132,
+    width: '100%',
+  },
 });
 
 function getTextColor(intervalType: IntervalType): string {
-    switch (intervalType) {
-        case 'exercise':
-            return Colors.RED;
-        case 'rest':
-            return Colors.BLUE;
-        default:
-            return Colors.WHITE;
-    }
+  switch (intervalType) {
+    case 'exercise':
+      return Colors.RED;
+    case 'rest':
+      return Colors.BLUE;
+    default:
+      return Colors.WHITE;
+  }
 }
