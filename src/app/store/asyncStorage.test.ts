c0fc247fd749ac87c1aab8452f938dd785d9734f
Irfan Undevli <i.undevli@gmail.com>
2025-01-01 15:53:38 +0000

    Display stored exercise duration

--- src/app/store/asyncStorage.test.ts
+++ src/app/store/asyncStorage.test.ts
@@ -1,5 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { storeExerciseDuration } from './asyncStorage';
+import { readExerciseDuration, storeExerciseDuration } from './asyncStorage';
 
 describe('asyncStorage', () => {
   beforeEach(() => {
@@ -24,6 +24,37 @@ describe('asyncStorage', () => {
       await storeExerciseDuration(duration);
 
       expect(consoleErrorSpy).toHaveBeenCalledWith('Failed to save the exercise duration.', expect.any(Error));
+    });
+  });
+
+  describe('readExerciseDuration', () => {
+    it('reads the stored exercise duration', async () => {
+      const duration = { minutes: 5, seconds: 30 };
+      (AsyncStorage.getItem as jest.Mock).mockResolvedValueOnce(JSON.stringify(duration));
+
+      const result = await readExerciseDuration();
+
+      expect(result).toEqual(duration);
+      expect(AsyncStorage.getItem).toHaveBeenCalledWith('@exercise_duration');
+    });
+
+    it('returns default duration if no duration is stored', async () => {
+      (AsyncStorage.getItem as jest.Mock).mockResolvedValueOnce(null);
+
+      const result = await readExerciseDuration();
+
+      expect(result).toEqual({ minutes: 1, seconds: 0 });
+      expect(AsyncStorage.getItem).toHaveBeenCalledWith('@exercise_duration');
+    });
+
+    it('handles errors when reading the exercise duration', async () => {
+      const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+      (AsyncStorage.getItem as jest.Mock).mockRejectedValueOnce(new Error('AsyncStorage error'));
+
+      const result = await readExerciseDuration();
+
+      expect(result).toEqual({ minutes: 1, seconds: 0 });
+      expect(consoleErrorSpy).toHaveBeenCalledWith('Failed to fetch the exercise duration.', expect.any(Error));
     });
   });
 });
--- src/app/store/asyncStorage.ts
+++ src/app/store/asyncStorage.ts
@@ -5,11 +5,23 @@ interface Duration {
   seconds: number;
 }
 
+const DEFAULT_DURATION: Duration = { minutes: 1, seconds: 0 };
+
 export const storeExerciseDuration = async (time: Duration): Promise<void> => {
   try {
     const jsonValue = JSON.stringify(time);
     await AsyncStorage.setItem('@exercise_duration', jsonValue);
   } catch (e) {
     console.error('Failed to save the exercise duration.', e);
+  }
+};
+
+export const readExerciseDuration = async (): Promise<Duration> => {
+  try {
+    const jsonValue = await AsyncStorage.getItem('@exercise_duration');
+    return jsonValue != null ? JSON.parse(jsonValue) : DEFAULT_DURATION;
+  } catch (e) {
+    console.error('Failed to fetch the exercise duration.', e);
+    return DEFAULT_DURATION;
   }
 };
