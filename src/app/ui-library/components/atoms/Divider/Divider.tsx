0ff08d9a957fa9325fcba7776bb54da83a84d3eb
Irfan Undevli <i.undevli@gmail.com>
2025-01-06 17:42:51 +0000

    Refactor timer screen design

--- src/app/ui-library/components/atoms/Divider/Divider.tsx
+++ src/app/ui-library/components/atoms/Divider/Divider.tsx
@@ -1,15 +1,24 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ViewStyle } from 'react-native';
 import { Colors } from '../../../styles';
 
 interface Props {
   color?: string;
+  spaceStyle?: Pick<ViewStyle, 'marginVertical' | 'paddingHorizontal'>;
+  height?: number;
 }
 
-export default function Divider({ color = Colors.WHITE }: Props) {
-  return <View style={[styles.divider, { backgroundColor: color }]}></View>;
+export default function Divider({ color = Colors.WHITE, height = 1, spaceStyle }: Props) {
+  return (
+    <View style={[styles.container, spaceStyle]}>
+      <View style={[styles.divider, { backgroundColor: color }, { height }]}></View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   divider: {
     height: 1,
     width: '100%',
